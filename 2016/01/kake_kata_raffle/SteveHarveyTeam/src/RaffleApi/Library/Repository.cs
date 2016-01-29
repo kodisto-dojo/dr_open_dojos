b19e5cbe149c11305233da4998c5e993883a80d8
jcmordan <juan.mordan@gmail.com>
2016-01-28 22:03:43 -0400

    solution

---
+++ 2016/01/kake_kata_raffle/SteveHarveyTeam/src/RaffleApi/Library/Repository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace RaffleApi.Library
+{
+    internal class Repository
+    {
+        internal static List<string> GetPlayer()
+        {
+            List<string> players = new List<string>();
+
+            var lines = File.ReadAllLines("tickets.dat");
+            foreach (string item in lines)
+            {               
+                var names = item.Split('=');
+                var tickets = int.Parse(names[1]);
+                for (int i = 0; i < tickets; i++)
+                {
+                    players.Add(names[0]);
+                }                    
+            }
+
+            return players;
+        }
+    }
+}
