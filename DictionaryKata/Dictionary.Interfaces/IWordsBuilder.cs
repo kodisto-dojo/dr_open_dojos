3683acbce7ecd2c42793d945ba0ce80154067a9c
EddyLeon <eddyleon21@gmail.com>
2016-02-27 21:13:47 -0400

    Eddy, Mordan y Estrella

---
+++ DictionaryKata/Dictionary.Interfaces/IWordsBuilder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary.Interfaces
+{
+    public interface IWordsBuilder
+    {
+       IList<string> GetCombinedWords(Func<String, bool> filter);
+    }
+}
