d909133a5f03b80993ec6ca5ad82667a25c6850d
EddyLeon <eddyleon21@gmail.com>
2016-02-27 21:16:44 -0400

    Joel, Mordan, and Eddy (C#)

---
+++ 2016/02/Extensible/Dictionary.Interfaces/IWordsDictionary.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary.Interfaces
+{
+    public interface IWordsDictionary 
+    {    
+           
+        IList<string> GetAllWrods();
+        IList<string> GetFilterWords(Func<string, bool> fiter);            
+    }
+}
